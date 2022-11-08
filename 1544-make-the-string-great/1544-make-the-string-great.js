d505127d9710cb1e6770f573f7e6ee6a1d3edaab
nms2811 <53978919+nms2811@users.noreply.github.com>
2022-11-08 00:56:42 -0500

    Time: 149 ms (8.87%), Space: 44.9 MB (37.23%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.js
+++ 1544-make-the-string-great/1544-make-the-string-great.js
@@ -3,15 +3,14 @@
  * @return {string}
  */
 var makeGood = function(s) {
-    s = s.split('');
     let i = 0;
-    while(i < s.length - 1) {
-        if (s[i] && s[i + 1] && s[i].toLowerCase() === s[i + 1].toLowerCase() && s[i] !== s[i + 1]) {
-            s.splice(i, 2);
-            i--;
-            continue;
+    while ( i < s.length - 1) {
+        if(Math.abs(s.charCodeAt(i) - s.charCodeAt(i+1)) === 32) {
+           s = s.slice(0, i) + s.slice(i+2); 
+           i--;
+        } else {
+            i++;
         }
-        i++;
     }
-    return s.join('')
+   return s;
 };
