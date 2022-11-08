2fe185899f75c0c4b6c46739b42bebbd8d1f28b0
nms2811 <53978919+nms2811@users.noreply.github.com>
2022-11-08 00:30:06 -0500

    Time: 96 ms (56.66%), Space: 42.5 MB (19.26%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -3,13 +3,32 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
  
-var sortColors = function(nums) {
-        let i,j,key;
-        for (i = 1; i < nums.length;i++) {
-        key = nums[i];
-        for(j = i-1; j >= 0 && nums[j] > key; j--) {
-            nums[j+1] = nums[j];
+var sortColors = function(nums, low, high) {
+    function swap(arr, i, j) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    function partition (arr, low, high) {
+        let pivot = arr[high];
+        let i = low - 1;
+
+        for (let j = low; j <= high-1; j++) {
+            if(arr[j] < pivot) {
+                i++;
+                swap(arr,i,j);
+            }
         }
-        nums[j+1] = key;
-    }   
+        swap(arr,i+1,high);
+        return(i+1);
+    }
+    function quicksort(nums, low, high){
+        if (low < high) {
+            let pi = partition(nums, low, high);
+            quicksort(nums, low, pi - 1);
+            quicksort(nums, pi + 1, high);
+        }
+    }
+    quicksort(nums, 0, nums.length - 1);
+    
 };
