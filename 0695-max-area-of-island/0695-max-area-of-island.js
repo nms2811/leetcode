73a77c2f31fc56ad71bbdfde8408dd85111cae31
nms2811 <53978919+nms2811@users.noreply.github.com>
2022-11-12 17:06:56 -0500

    Time: 117 ms (77.75%), Space: 45.2 MB (65.51%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.js
+++ 0695-max-area-of-island/0695-max-area-of-island.js
@@ -7,31 +7,26 @@ var maxAreaOfIsland = function(grid) {
     let h = grid.length;
     let w = h && grid[0].length;
     let max = 0;
-    let count = 0;
-    let check;
     for(let i = 0; i < h; i++) {
         for(let j = 0; j < w; j++) {
-            if (grid[i][j] === 0) continue;
-            check = dfs(i,j);
-            if(max < check) max = check;
-            count = 0;
+            if (grid[i][j] === 1) {
+                max = Math.max(max, dfs(i, j));
+            }
         }
     }
     return max;
 
     function dfs(r,c) {
+        let count = 0;
+        if (r < 0 || c < 0 || r >= h || c >= w || grid[r][c] === 0) return 0;
 
-        if (r < 0 || c < 0 || r >= h || c >= w) return;
+        grid[r][c] = 0;
+        count++;
+        count += dfs(r-1, c);
+        count += dfs(r+1, c);
+        count += dfs(r, c-1);
+        count += dfs(r, c+1);
 
-        if(grid[r][c] === 1) {
-            count++;
-            grid[r][c] = 0;
-
-            dfs(r-1, c);
-            dfs(r+1, c);
-            dfs(r, c-1);
-            dfs(r, c+1);
-        }
         return count;
     }
 };
