3a6aa321863d8b1661f52f9218abbb6da5052733
nms2811 <53978919+nms2811@users.noreply.github.com>
2022-11-12 17:27:36 -0500

    Time: 77 ms (85.31%), Space: 45.5 MB (8.93%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let low = 0;
+    let high = nums.length -1;
+    while(low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        if(nums[mid] === target) return mid;
+        else if (nums[mid] < target) low = mid + 1;
+        else if (nums[mid] > target) high = mid - 1;
+    }
+    return -1;
+};
