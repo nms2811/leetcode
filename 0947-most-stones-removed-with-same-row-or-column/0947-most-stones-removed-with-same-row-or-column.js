7861f6240ad603c9eb268cd35dfc4286fab64fac
nms2811 <53978919+nms2811@users.noreply.github.com>
2022-11-14 19:53:12 -0500

    Time: 526 ms (8.72%), Space: 100.1 MB (5.23%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[][]} stones
+ * @return {number}
+ */
+var removeStones = function(stones) {
+    let storage = {};
+    let visited = {};
+    let count = 0;
+    for( i in stones) {
+        storage[i] = {}
+        visited[i] = false;
+    }
+    
+    for(let i = 0; i < stones.length; i++) { 
+        for(let j = 0; j < stones.length; j++) { 
+            if(i !== j) {
+                if (stones[i][0] === stones[j][0] || stones[i][1] === stones[j][1]) { 
+                    storage[i][j] = true;
+                    storage[j][i] = true;
+                }
+            }
+        }
+    }
+    for(let e in storage) {
+        dfs(e, visited);
+    }
+    return count;
+    
+    function dfs(node, visited) {
+        visited[node] = true;
+        let edges = storage[node];
+        for(let e in edges) {
+            if(!visited[e]) {
+                count++;
+                dfs(e, visited);
+            }
+        }
+    }
+}
