74aabc0241332d314695a2bb579e4041d93a71b5
nms2811 <53978919+nms2811@users.noreply.github.com>
2022-11-16 22:40:49 -0500

    Time: 268 ms (12.16%), Space: 47.1 MB (52.70%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.js
+++ 0223-rectangle-area/0223-rectangle-area.js
@@ -12,19 +12,11 @@
 var computeArea = function(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2) {
     let r1 = (ax2 - ax1) * (ay2 - ay1);
     let r2 = (bx2 - bx1) * (by2 - by1);
-    let w = 0;
-    let h = 0;
-    if (ax2 <= bx1 || bx2 <= ax1 || ay2 <= by1 || by2 <= ay1) return r1 + r2;
-    if(ax1 <= bx1) {
-        w = (ax2 <= bx2)? ax2 - bx1: bx2 - bx1;
+    let xoverlap = Math.min(ax2, bx2) - Math.max(ax1, bx1);
+    let yoverlap = Math.min(ay2, by2) - Math.max(ay1, by1);
+    let overlap = 0;
+    if(xoverlap > 0 && yoverlap > 0) overlap = xoverlap * yoverlap;
 
-    }else {
-        w = (ax2 <= bx2)? ax2 - ax1: bx2 - ax1;
-    }
-    if(by1 <= ay1) {
-        h = (ay2 <= by2)? ay2 - ay1: by2 - ay1;
-    }else {
-        h = (ay2 <= by2)? ay2 - by1: by2 - by1;
-    }
-    return r1 + r2 - (w * h);
+    return r1 + r2 - overlap
+   
 };
