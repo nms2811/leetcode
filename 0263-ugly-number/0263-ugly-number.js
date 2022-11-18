99554bd2c8bc768655abb340da75caff5f6aaea5
nms2811 <53978919+nms2811@users.noreply.github.com>
2022-11-17 22:08:55 -0500

    Time: 106 ms (71.32%), Space: 43.6 MB (48.27%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isUgly = function(n) {
+    if (n <= 0) return false;
+    while (n > 0) {
+        if(n % 2 === 0) n = n / 2;
+        else if(n % 3 === 0) n = n / 3;
+        else if(n % 5 === 0) n = n / 5;
+        else if(n === 1) return true;
+        else {
+            return false;
+        }
+    }
+
+
+};
