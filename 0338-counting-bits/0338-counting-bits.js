7bf8fcd3eb6c1be2d8e4271fce0d69bd6114a4b6
nms2811 <53978919+nms2811@users.noreply.github.com>
2022-11-22 10:47:01 -0500

    Time: 128 ms (78.94%), Space: 49.3 MB (49.09%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.js
+++ 0338-counting-bits/0338-counting-bits.js
@@ -3,14 +3,13 @@
  * @return {number[]}
  */
 var countBits = function(n) {
-    dp = new Array(n + 1).fill(-1);;
-    dp[0] = 0;
-    let q = 0;
-    let a = 0;
+    dp = [0];
     for(let i = 1;i <=n; i++) {
-        let q = i%2;
-        let a = Math.floor(i/2);
-        dp[i] = dp[a] + q;
+       if(i % 2 == 0){
+           dp[i] = dp[i / 2];
+       } else {
+           dp[i] = dp[Math.floor(i / 2)] + 1
+       }
     }
 
     return dp;
