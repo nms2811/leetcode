cff9ed6a1173b3e72e59df565f6051214f2c119a
nms2811 <53978919+nms2811@users.noreply.github.com>
2022-11-22 12:05:51 -0500

    Time: 226 ms (6.23%), Space: 42 MB (44.14%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -3,12 +3,11 @@
  * @return {number}
  */
 var fib = function(n) {
-    dp = [];
-    dp[0] = 0;
-    dp[1] = 1;
-    for(let i = 2; i <= n; i++) {
-        dp[i] = dp[i - 1] + dp[i - 2];
-    }
-
-    return dp[n];
+    return dp(n);
+    function dp(n) {
+        if (n === 0) return 0; 
+	    if (n === 1) return 1;
+        let temp = dp(n - 1) + dp(n - 2);
+        return temp;
+    }  
 };
