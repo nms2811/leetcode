9bf9dd0e92f783cebb4c8f20d0610b158254272f
nms2811 <53978919+nms2811@users.noreply.github.com>
2022-11-22 09:49:50 -0500

    Time: 704 ms (25.72%), Space: 44.9 MB (45.71%) - LeetHub

---
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var getMoneyAmount = function(n) {
+    
+    let dp = Array(n+1).fill().map(() => Array(n+1).fill(Infinity));
+    return recurse(1, n);
+
+    function recurse(l, h) {
+        if(h <= l) return 0;
+        if(dp[l][h] !== Infinity) return dp[l][h];
+        for(let i = l; i <= h; i++) {
+            dp[l][h] = Math.min(dp[l][h], i + Math.max(recurse(l, i - 1), recurse(i + 1, h)));
+        }
+        return dp[l][h];
+    }
+
+};
