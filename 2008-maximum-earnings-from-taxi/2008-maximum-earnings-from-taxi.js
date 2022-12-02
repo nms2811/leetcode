768621fe162c3f372949e67c76c6757681c1cda6
nms2811 <53978919+nms2811@users.noreply.github.com>
2022-12-02 01:55:38 -0500

    Time: 7277 ms (8.20%), Space: 66.2 MB (95.08%) - LeetHub

--- 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.js
+++ 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.js
@@ -4,19 +4,15 @@
  * @return {number}
  */
 var maxTaxiEarnings = function(n, rides) {
-    let customers = [];
     let rl = rides.length;
-    for(let i = 0; i < rl; i++) {
-        customers.push({s: rides[i][0], e: rides[i][1], t:rides[i][2]});
-    }
-    customers.sort((a,b) => a.e - b.e);
+    rides.sort((a,b) => a[1] - b[1]);
     let dp = Array(rl);
-    dp[0] = customers[0].e - customers[0].s + customers[0].t;;
+    dp[0] = rides[0][1] - rides[0][0] + rides[0][2];
     for (let i = 1; i < rl; i++) {
-        let total = customers[i].e - customers[i].s + customers[i].t;
+        let total = rides[i][1] - rides[i][0] + rides[i][2];
         let task = -1;
         for(let j = i-1; j >= 0; j--) {
-            if(customers[j].e <= customers[i].s) {
+            if(rides[j][1] <= rides[i][0]) {
                 task = j;
                 break;
             }
