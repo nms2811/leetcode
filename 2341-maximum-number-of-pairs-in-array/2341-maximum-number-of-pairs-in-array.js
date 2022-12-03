95ff36c31fac2e8b76a1f0a6188d1c8c9524ba6a
nms2811 <53978919+nms2811@users.noreply.github.com>
2022-12-03 18:51:30 -0500

    Time: 90 ms (67.83%), Space: 42.6 MB (30.57%) - LeetHub

--- 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.js
+++ 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.js
@@ -3,16 +3,18 @@
  * @return {number[]}
  */
 var numberOfPairs = function(nums) {
-    let count = 0;
-    for(let i = 0; i < nums.length; i++) {
-        let index1 = nums.indexOf(nums[i]);
-        let index2 = nums.lastIndexOf(nums[i]);
-        if(index1 !== index2) {
-            nums.splice(index2, 1);
-            nums.splice(index1, 1);
-            count++;
-            i--;
+    let answer = [0, 0];
+    let check = new Set();
+    
+    for (const n of nums) {
+        if (check.has(n)) {
+            answer[0]++;
+            check.delete(n);
+        } else {
+            check.add(n);
         }
     }
-    return [count, nums.length];
+    answer[1] = check.size;
+    
+    return answer;
 };
