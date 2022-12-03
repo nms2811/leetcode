48276eae338d8096e84c1db263a6119f651ba8b6
nms2811 <53978919+nms2811@users.noreply.github.com>
2022-12-02 23:01:59 -0500

    Time: 134 ms (78.99%), Space: 45.2 MB (70.25%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -3,13 +3,8 @@
  * @return {number}
  */
 var firstUniqChar = function(s) {
-    let map = new Map();
     for(let i = 0; i < s.length; i++) {
-        map.set(s[i], map.get(s[i]) + 1 || 1);
-    }
-
-    for(let i = 0; i < s.length; i++) {
-        if(map.get(s[i]) === 1) return i;
+        if(s.indexOf(s[i]) === s.lastIndexOf(s[i])) return i;
     }
     return -1;
     
