91a2357891f759ad6537c34f3f07b479bde02748
nms2811 <53978919+nms2811@users.noreply.github.com>
2022-12-04 01:05:46 -0500

    Time: 134 ms (77.59%), Space: 66.2 MB (20.69%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.js
+++ 2256-minimum-average-difference/2256-minimum-average-difference.js
@@ -5,13 +5,17 @@
 var minimumAverageDifference = function(nums) {
     let total = nums.reduce((sum, a) => sum + a, 0);
     let n = nums.length;
-    let lsum = 0;
-    let rsum = 0;
+    let sum = [];
     let min = [];
-    for(let i = 0; i < n; i++) {
-        lsum += nums[i];
-        rsum = total - lsum;
-        min[i] = (n-1-i !== 0)? Math.abs(Math.floor(lsum/(i+1)) - Math.floor(rsum/(n - i - 1))): Math.abs(Math.floor(lsum/(i+1)));
+    sum[0] = total - nums[0];
+    for(let i = 1; i < n; i++) {
+        sum[i] = sum[i - 1] - nums[i];
     }
+    for(let i = 0; i < n; i++) {
+        let a = Math.floor((total - sum[i])/(i+1));
+        let b = (n - i - 1 !== 0)?Math.floor(sum[i]/(n - i - 1)): 0;
+        min[i] = Math.abs(a - b);
+    }
+
     return min.indexOf(Math.min(...min));
 };
