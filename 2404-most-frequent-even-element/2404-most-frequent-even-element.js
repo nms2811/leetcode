f95a29aa0fbfeb3cd1377793c08722f6f3807784
nms2811 <53978919+nms2811@users.noreply.github.com>
2022-12-04 01:54:49 -0500

    Time: 118 ms (95.43%), Space: 49.8 MB (73.56%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.js
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.js
@@ -4,7 +4,7 @@
  */
 var mostFrequentEven = function(nums) {
     if(nums.length==1 && nums[0]%2===0) return nums[0];
-    map = new Map();
+    let map = new Map();
     for(let i = 0; i < nums.length; i++) {
         if(nums[i] % 2 === 0) map.set(nums[i], map.get(nums[i]) + 1 || 1)
     }
