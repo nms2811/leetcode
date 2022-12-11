24ca69f86e438dd4ecc84a7548bc83603b6df336
nms2811 <53978919+nms2811@users.noreply.github.com>
2022-12-10 23:10:44 -0500

    Time: 145 ms (28.23%), Space: 51.1 MB (94.00%) - LeetHub

---
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxPathSum = function(root) {
+    let max = -Infinity;
+    dfs(root);
+    function dfs(node) {
+        if(!node) return 0;
+        let lsum = Math.max(0, dfs(node.left));
+        let rsum = Math.max(0, dfs(node.right));
+        max = Math.max(max, node.val + lsum + rsum);
+        return Math.max(lsum, rsum) + node.val;
+    }
+    return max;
+};
