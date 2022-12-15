de871284acfc6d423f444811d6b7f51647bc6616
nms2811 <53978919+nms2811@users.noreply.github.com>
2022-12-14 20:33:34 -0500

    Time: 117 ms (84.58%), Space: 56.8 MB (39.11%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -0,0 +1,14 @@
+// /**
+//  * @param {string} text1
+//  * @param {string} text2
+//  * @return {number}
+//  */
+var longestCommonSubsequence = function(text1, text2) {
+    let dp = new Array(text1.length + 1).fill(0).map(() => new Array(text2.length + 1).fill(0));
+    for(let i = 1; i < dp.length; i++) {
+        for(let j = 1; j < dp[0].length; j++) {
+            dp[i][j] = (text1[i-1] === text2[j-1])? 1 + dp[i-1][j-1]: Math.max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+    return dp[text1.length][text2.length];
+};
