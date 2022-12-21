c8ef467bcd7e148ac11ca84c579b08e1391d7cf3
nms2811 <53978919+nms2811@users.noreply.github.com>
2022-12-20 23:23:39 -0500

    Time: 108 ms (38.05%), Space: 44 MB (76.11%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[][]} rooms
+ * @return {boolean}
+ */
+var canVisitAllRooms = function(rooms) {
+    let visited = new Set();
+    dfs(rooms, 0);
+    return rooms.length === visited.size;
+    function dfs(rooms, idx) {
+        visited.add(idx);
+        let a = rooms[idx];
+        for(let key of a)    
+            if(!visited.has(key))dfs(rooms, key) 
+    }
+}
