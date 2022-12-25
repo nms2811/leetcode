b7052ab092cdda94d2e6eded9855cee74e08dcb5
nms2811 <53978919+nms2811@users.noreply.github.com>
2022-12-25 01:19:10 -0500

    Time: 95 ms (83.05%), Space: 52 MB (25.34%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let curr = prices[0];
+    let res = 0;
+    for(let i = 0; i < prices.length; i++){
+        if(prices[i] > curr && prices[i] - curr > res) {
+            res = prices[i] - curr;
+        }
+        if(prices[i] < curr) {
+            curr = prices[i];
+        }
+    }
+    return res;
+};
