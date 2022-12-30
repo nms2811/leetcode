895052409e35e07928d88f88c431979f4346b71d
nms2811 <53978919+nms2811@users.noreply.github.com>
2022-12-30 18:38:23 -0500

    Time: 326 ms (5.13%), Space: 52.2 MB (26.02%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[][]} graph
+ * @return {number[][]}
+ */
+var allPathsSourceTarget = function(graph) {
+    let output = [];
+    let path = [];
+    path.push(0);
+    dfs(0);
+    return output;
+    function dfs(node) {
+        if(node === graph.length - 1) {
+            console.log('***')
+            console.log(path);
+            console.log('***')
+            output.push(path.slice(0));
+        } else {
+            for(let i = 0; i< graph[node].length; i++){ 
+                path.push(graph[node][i]);
+                dfs(graph[node][i]);
+                path.pop(); 
+            }
+        }
+    }
+};
