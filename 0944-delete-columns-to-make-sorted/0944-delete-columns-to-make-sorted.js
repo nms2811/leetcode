fe8fed44b553cc8ced4c2c05b90d881f30b8efae
nms2811 <53978919+nms2811@users.noreply.github.com>
2023-01-03 13:08:16 -0500

    Time: 80 ms (81.31%), Space: 45.1 MB (99.07%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.js
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.js
@@ -5,8 +5,8 @@
 var minDeletionSize = function(strs) {
     let count = 0;
     for(let i = 0; i < strs[0].length; i++) {
-        for(let j = 1; j < strs.length; j++){
-            if(strs[j-1].charAt(i) > strs[j].charAt(i)) {
+        for(let j = 0; j < strs.length - 1; j++){
+            if(strs[j].charAt(i) > strs[j+1].charAt(i)) {
                 count++;
                 break;
             }
