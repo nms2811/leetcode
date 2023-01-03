082f8c92331bed1bbb95033749633a20efc9f143
nms2811 <53978919+nms2811@users.noreply.github.com>
2023-01-03 13:10:09 -0500

    Time: 73 ms (76.42%), Space: 42.1 MB (79.67%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var detectCapitalUse = function(word) {
+    let upper = word.toUpperCase();
+    let lower = word.toLowerCase();
+    let first = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    if(upper === word || lower === word || first === word) return true;
+    else return false;
+};
