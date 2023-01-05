66c69cf9a92a9d20ed62e0a6b5711623ef9b2ad6
nms2811 <53978919+nms2811@users.noreply.github.com>
2023-01-04 21:39:50 -0500

    Time: 356 ms (58.62%), Space: 66.8 MB (86.21%) - LeetHub

---
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var findMinArrowShots = function(points) {
+    let count = 0, currentEnd = -Infinity;
+    points = points.sort((a,b) => a[0] - b[0]);
+    for(let [xstart, xend] of points) {
+        if(xstart > currentEnd) {
+            count++;
+            currentEnd = xend;
+        } else {
+            currentEnd = Math.min(currentEnd, xend);
+        }
+    }
+    return count;
+};
