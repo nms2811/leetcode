2424cd6bb7f834b4775263960a115a340e52d72e
nms2811 <53978919+nms2811@users.noreply.github.com>
2023-01-05 22:14:01 -0500

    Time: 376 ms (45.00%), Space: 56 MB (45.00%) - LeetHub

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} costs
+ * @param {number} coins
+ * @return {number}
+ */
+var maxIceCream = function(costs, coins) {
+    costs = costs.sort((a,b)=> a - b);
+    let count = 0;
+    for(let i = 0; i < costs.length; i++) {
+        if(costs[i] <= coins) {
+            count++;
+            coins -= costs[i];
+        }
+    }
+    return count;
+};
