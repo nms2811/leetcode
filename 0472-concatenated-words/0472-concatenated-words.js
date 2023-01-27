5a66250b282d5c136790226cbe76e7fb1b76aa7c
nms2811 <53978919+nms2811@users.noreply.github.com>
2023-01-26 23:01:27 -0500

    Time: 182 ms (81.13%), Space: 54.5 MB (77.36%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.js
+++ 0472-concatenated-words/0472-concatenated-words.js
@@ -6,19 +6,20 @@ var findAllConcatenatedWordsInADict = function(words) {
     let set = new Set(words);
     let res = [];
 
-    let dfs = function (word, n) {
-        if (n >= 2 && word.length == 0) return true
-        for (let i = 1; i <= word.length; i++) {
-            let substr = word.substring(0, i)
-            if (set.has(substr)) {
-                if (dfs(word.slice(i), n+1)) return true
-            }
-        }
-        return false
+    for(const word of words) {
+        if(dfs(word, 0)) res.push(word);
     }
 
-    for (let word of words) {
-        if(dfs(word, 0)) res.push(word)
+    return res;
+
+    function dfs(word, n) {
+        if(n >= 2 && word.length === 0) return true;
+        for(let i = 1; i <= word.length; i++) {
+            let substr = word.substring(0, i);
+            if(set.has(substr)){
+                if(dfs(word.slice(i), n + 1)) return true;
+            }
+        }
+        return false;
     }
-    return res
 };
