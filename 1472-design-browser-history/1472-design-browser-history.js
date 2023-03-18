47eb18facd059bce3426a84eb527cd37e0bdad86
nms2811 <53978919+nms2811@users.noreply.github.com>
2023-03-18 01:13:37 -0400

    Time: 587 ms (5.21%), Space: 54.7 MB (5.54%) - LeetHub

---
+++ 1472-design-browser-history/1472-design-browser-history.js
@@ -0,0 +1,43 @@
+class BrowserHistory{
+    constructor(homepage) {
+        this.pages = {
+            url: homepage,
+            back: null,
+            next: null
+        };
+    }
+
+    visit(url) {
+        // console.log(this.pages);
+        this.pages.next = {
+            url,
+            back: this.pages,
+            next:null
+        }
+        this.pages = this.pages.next;
+        console.log(this.pages);
+    }
+
+    back(steps) {
+        while(steps && this.pages.back) {
+            this.pages = this.pages.back;
+            steps--;
+        }
+        return this.pages.url;
+    }
+
+    forward(steps) {
+        while(steps && this.pages.next) {
+            this.pages = this.pages.next;
+            steps--;
+        }
+        return this.pages.url;
+    }
+}
+/** 
+ * Your BrowserHistory object will be instantiated and called as such:
+ * var obj = new BrowserHistory(homepage)
+ * obj.visit(url)
+ * var param_2 = obj.back(steps)
+ * var param_3 = obj.forward(steps)
+ */
