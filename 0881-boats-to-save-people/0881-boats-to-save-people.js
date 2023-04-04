405574f79282625ee8c4210b6c36e51850879c8f
nms2811 <53978919+nms2811@users.noreply.github.com>
2023-04-03 22:23:24 -0400

    Time: 157 ms (75.68%), Space: 49.8 MB (65.64%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} people
+ * @param {number} limit
+ * @return {number}
+ */
+const numRescueBoats = function(people, limit) {
+  let count = 0;
+  let n = people.length;
+  let i = 0;
+  
+  people.sort((a, b) => b - a);
+  while (i < n) {
+    if (people[i] + people[n - 1] <= limit) {
+      n--;
+    }
+    i++;
+    count++;
+  }
+
+  return count;
+};
